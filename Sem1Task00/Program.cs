b429117a87681ca348c7a91bd3905af79961e74d
Dimaneug <dimaneug@gmail.com>
2023-03-23 21:45:49 +0300

    Added 0 and 1 tasks

---
+++ Sem1Task00/Program.cs
@@ -0,0 +1,9 @@
+﻿string? inLine = Console.ReadLine();
+
+if (inLine != null) {
+    int inNumber = int.Parse(inLine);
+
+    int outNumber = inNumber*inNumber;
+    
+    Console.WriteLine(outNumber);
+}
---
+++ Sem1Task01/Program.cs
@@ -0,0 +1,13 @@
+﻿string? firstNumberLine = Console.ReadLine();
+string? secondNumberLine = Console.ReadLine();
+
+if (firstNumberLine != null && secondNumberLine != null) {
+    int inNumber1 = int.Parse(firstNumberLine);
+    int inNumber2 = int.Parse(secondNumberLine);
+
+    if (inNumber1 == (int)Math.Pow(inNumber2, 2)) {
+        Console.WriteLine("Yes");
+    }
+    else Console.WriteLine("No");
+
+}
