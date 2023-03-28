44ae0e85800b99d1889744e4bfe03c8b3ae8b454
Dimaneug <dimaneug@gmail.com>
2023-03-28 11:54:59 +0300

    Add task 6

---
+++ Sem1Task06/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введите число: ");
+string? line = Console.ReadLine();
+
+if (line != null) {
+    int num = int.Parse(line);
+
+    if (num % 2 == 0) Console.WriteLine("Да");
+    else Console.WriteLine("Нет");
+}
