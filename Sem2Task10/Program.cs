ecb8dd972f5d794acf94e7799800ce9206240d6e
Dimaneug <dimaneug@gmail.com>
2023-03-28 13:46:28 +0300

    Add task 10

---
+++ Sem2Task10/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Введите трёхзначное число: ");
+int num = int.Parse(Console.ReadLine()??"0");
+
+if (num>99 && num<1000) {
+    Console.WriteLine("Вторая цифра - {0}", (num/10)%10);
+}
+else
+    Console.WriteLine("Вы ввели не трёхзначное число!");
