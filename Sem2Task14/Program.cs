de794d37db495405d4919df80f68d15968c2cf7a
Dimaneug <dimaneug@gmail.com>
2023-03-28 13:32:31 +0300

    Add task 14

---
+++ Sem2Task14/Program.cs
@@ -0,0 +1,7 @@
+﻿int num = int.Parse(Console.ReadLine()??"0");
+bool res = (num%7==0 && num%23==0);
+
+if (res) 
+    Console.WriteLine("Число {0} кратно 7 и 23", num);
+else
+    Console.WriteLine("Число {0} не кратно 7 и 23", num);
