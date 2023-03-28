312a8234d001361493980d0e02c895c1aeb9314d
Dimaneug <dimaneug@gmail.com>
2023-03-28 12:52:54 +0300

    Add task 11

---
+++ Sem2Task11/Program.cs
@@ -0,0 +1,8 @@
+﻿int num = new Random().Next(100,1000);
+Console.WriteLine(num);
+
+int firstNum = num / 100;
+int lastNum = num % 10;
+int newNum = firstNum * 10 + lastNum;
+
+Console.WriteLine(newNum);
