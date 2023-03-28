22486a2260190e6a10319f2a93e10505c8f95fe1
Dimaneug <dimaneug@gmail.com>
2023-03-28 13:41:37 +0300

    Add task 16

---
+++ Sem2Task16/Program.cs
@@ -0,0 +1,12 @@
+﻿int num1 = int.Parse(Console.ReadLine()??"0");
+int num2 = int.Parse(Console.ReadLine()??"0");
+
+void SqrtTest(int a, int b) {
+    if (a == b*b)
+        Console.WriteLine(a+" квадрат "+b);
+    else
+        Console.WriteLine(a+" не квадрат "+b);
+}
+
+SqrtTest(num1, num2);
+SqrtTest(num2, num1);
