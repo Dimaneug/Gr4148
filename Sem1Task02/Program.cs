bbaf812df39ced3b0ea24cedde77b8efc8fb38e7
Dimaneug <dimaneug@gmail.com>
2023-03-28 11:41:37 +0300

    Add task 2

---
+++ Sem1Task02/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите первое число: ");
+string? firstLine = Console.ReadLine();
+
+Console.Write("Введите второе число: ");
+string? secondLine = Console.ReadLine();
+
+if (firstLine != null && secondLine != null) {
+    int firstNumber = int.Parse(firstLine);
+    int secondNumber = int.Parse(secondLine);
+
+    if (firstNumber > secondNumber) Console.WriteLine("Большее число - {0}", firstNumber);
+    else Console.WriteLine("Большее число - {0}", secondNumber);
+}
