a81b7f042008e127a6176a38d9aa5f7a570fa8f2
Dimaneug <dimaneug@gmail.com>
2023-03-28 13:59:29 +0300

    Add task 13

---
+++ Sem2Task13/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine()??"0");
+
+if ((num / 100)>0) 
+    while (num>99) {
+        if (num < 1000)
+            Console.WriteLine("Третья цифра - {0}", num%10);
+        num /= 10;
+    }
+else
+    Console.WriteLine("Третьей цифры нет");
