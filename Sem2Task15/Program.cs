a64b56e522d0fe832f1aa0c5b1088e03970d803a
Dimaneug <dimaneug@gmail.com>
2023-03-28 14:10:39 +0300

    Add task 15

---
+++ Sem2Task15/Program.cs
@@ -0,0 +1,18 @@
+﻿var daysOfWeek = new Dictionary<int, string>()
+{
+    [1] = "нет",
+    [2] = "нет",
+    [3] = "нет",
+    [4] = "нет",
+    [5] = "нет",
+    [6] = "да",
+    [7] = "да"
+};
+
+Console.Write("Введите день недели: ");
+int day = int.Parse(Console.ReadLine()??"0");
+
+if (0<day && day<8) 
+    Console.WriteLine(daysOfWeek[day]);
+else
+    Console.WriteLine("Такого дня недели не существует");
