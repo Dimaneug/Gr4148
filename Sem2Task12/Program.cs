b09a0e8dd0b22cafd97779ac43b73f18f46b785e
Dimaneug <dimaneug@gmail.com>
2023-03-28 13:32:12 +0300

    Add task 12

---
+++ Sem2Task12/Program.cs
@@ -0,0 +1,7 @@
+﻿int num1 = int.Parse(Console.ReadLine()??"0");
+int num2 = Convert.ToInt32(Console.ReadLine());
+bool res = (num1%num2==0);
+if (res) 
+    Console.WriteLine("Первое число кратно второму");
+else
+    Console.WriteLine("Первое число не кратно второму");
