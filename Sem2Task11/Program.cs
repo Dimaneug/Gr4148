36ae8b6c7dd9bbc5e4aa1c0decc2765e72ca4c47
Dimaneug <dimaneug@gmail.com>
2023-03-28 12:56:04 +0300

    Simplify task 11

--- Sem2Task11/Program.cs
+++ Sem2Task11/Program.cs
@@ -1,8 +1,8 @@
 ﻿int num = new Random().Next(100,1000);
 Console.WriteLine(num);
 
-int firstNum = num / 100;
-int lastNum = num % 10;
-int newNum = firstNum * 10 + lastNum;
+// int firstNum = num / 100;
+// int lastNum = num % 10;
+// int newNum = firstNum * 10 + lastNum;
 
-Console.WriteLine(newNum);
+Console.WriteLine((num/100)*10+(num%10));
