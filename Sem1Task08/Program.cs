69c2a132c48f3954d9ab713f9030bcec65caeb3a
Dimaneug <dimaneug@gmail.com>
2023-03-28 12:01:03 +0300

    Add task 8

---
+++ Sem1Task08/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите число: ");
+string? line = Console.ReadLine();
+
+if (line != null) {
+    int num = int.Parse(line);
+
+    for (int i = 2; i <= num; i += 2) {
+        Console.Write("{0} ", i);
+    }
+
+    Console.WriteLine();
+
+}
