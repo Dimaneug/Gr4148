34b3d723a8871d3f0a2b24b26689e82c797e6902
Dimaneug <dimaneug@gmail.com>
2023-04-01 11:42:52 +0300

    Add task 20

---
+++ Sem3Task20/Program.cs
@@ -0,0 +1,18 @@
+﻿// Метод читает данные от пользователя
+int ReadData(string msg) 
+{
+    Console.Write(msg);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+
+// Вычисляет расстояние между точками в 2D
+void CalcDistance2D(int x1, int y1, int x2, int y2)
+{
+    Console.WriteLine(Math.Sqrt((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2)));
+}
+
+int x1 = ReadData("Введите x1: ");
+int y1 = ReadData("Введите y1: ");
+int x2 = ReadData("Введите x2: ");
+int y2 = ReadData("Введите y2: ");
+CalcDistance2D(x1, y1, x2, y2);
