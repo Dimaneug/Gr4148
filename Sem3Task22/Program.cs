c7ed08709428ac46592cf5ab04ba5111c0a63bcd
Dimaneug <dimaneug@gmail.com>
2023-04-01 11:54:53 +0300

    Add task 22

---
+++ Sem3Task22/Program.cs
@@ -0,0 +1,19 @@
+﻿// Метод читает данные от пользователя
+int ReadData(string msg)
+{
+    Console.Write(msg);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+
+// Генерирует строку с N числами в pow степени
+string LineGen(int N, int pow)
+{
+    string res = "";
+    for (int i = 1; i <= N; i++)
+        res += Math.Pow(i, pow) + " ";
+    return res;
+}
+
+int N = ReadData("Введите N: ");
+Console.WriteLine(LineGen(N, 1));
+Console.WriteLine(LineGen(N, 2));
