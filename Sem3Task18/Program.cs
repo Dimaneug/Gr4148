ac1b4c98a8f98da424317c186fbae08e9e2f38a3
Dimaneug <dimaneug@gmail.com>
2023-04-01 11:32:52 +0300

    Add task 18

---
+++ Sem3Task18/Program.cs
@@ -0,0 +1,21 @@
+﻿// Метод читает данные от пользователя
+int ReadData(string msg) 
+{
+    Console.Write(msg);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+
+// Метод выводит диапазон координат в заданной четверти
+void PrintAnswer(int quarter)
+{
+    if (quarter == 1) Console.WriteLine("X>0, Y>0");
+    if (quarter == 2) Console.WriteLine("X>0, Y<0");
+    if (quarter == 3) Console.WriteLine("X<0, Y<0");
+    if (quarter == 4) Console.WriteLine("X<0, Y>0");
+}
+
+int quarter = ReadData("Введите номер четверти: ");
+if (quarter > 0 && quarter < 5)
+    PrintAnswer(quarter);
+else
+    Console.WriteLine("Неправильный номер четверти");
