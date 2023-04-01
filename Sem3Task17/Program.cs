8129f0c31bc155beac0cb62a1ec9e98a17c92b4e
Dimaneug <dimaneug@gmail.com>
2023-04-01 11:24:26 +0300

    Add task 17

---
+++ Sem3Task17/Program.cs
@@ -0,0 +1,20 @@
+﻿// Метод читает данные от пользователя
+int ReadData(string msg) 
+{
+    Console.Write(msg);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+
+// Метод отправляет четверть по координатам точки
+void PrintQuarterTest(int x, int y) 
+{
+    if (x>0 && y>0) Console.WriteLine("Точка в 1 четверти");
+    if (x>0 && y<0) Console.WriteLine("Точка в 2 четверти");
+    if (x<0 && y<0) Console.WriteLine("Точка в 3 четверти");
+    if (x<0 && y>0) Console.WriteLine("Точка в 4 четверти");
+
+}
+
+int coordX = ReadData("Введите X: ");
+int coordY = ReadData("Введите Y: ");
+PrintQuarterTest(coordX, coordY);
