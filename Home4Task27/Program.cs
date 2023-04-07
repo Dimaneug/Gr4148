fa3267948bb272fdd599b71a874f8b9071046f8e
Dimaneug <dimaneug@gmail.com>
2023-04-07 11:16:34 +0300

    Add task 27

---
+++ Home4Task27/Program.cs
@@ -0,0 +1,30 @@
+﻿// Напишите программу, которая принимает на вход 
+// число и выдаёт сумму цифр в числе
+
+// Метод читает данные от пользователя
+int ReadData(string msg)
+{
+    Console.Write(msg);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+
+// Метод вывода результата
+void PrintResult(string msg, int res)
+{
+    Console.WriteLine(msg + res);
+}
+
+// Метод подсчёта суммы цифр в числе
+int SumDigit(int num) 
+{
+    int res = 0;
+    while (num > 0) {
+        res += num%10;
+        num /= 10;
+    }
+    return res;
+}
+
+
+int num = ReadData("Введите число: ");
+PrintResult("Сумма цифр: ", SumDigit(num));
