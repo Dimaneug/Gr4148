8436c525b1c1148300ded7c544850d2c4140013d
Dimaneug <dimaneug@gmail.com>
2023-04-07 10:06:56 +0300

    Add task 26

---
+++ Sem4Task26/Program.cs
@@ -0,0 +1,29 @@
+﻿// Напишите программу, которая принимает на вход число 
+// и выдаёт количество цифр в числе
+
+// Метод читает данные от пользователя
+int ReadData(string msg)
+{
+    Console.Write(msg);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+
+// Метод вывода результата
+void PrintResult(string msg, int res)
+{
+    Console.WriteLine(msg + res);
+}
+
+// Метод подсчёта цифр в числе
+int CountDigit(int num) 
+{
+    int res = 0;
+    while (num > 0) {
+        res += 1;
+        num /= 10;
+    }
+    return res;
+}
+
+int num = ReadData("Введите число: ");
+PrintResult("Количество цифр: ", CountDigit(num));
