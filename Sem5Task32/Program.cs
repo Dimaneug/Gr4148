689b14a3af8b8f6bc9e733c8f454aba3c79f0a4e
Dimaneug <dimaneug@gmail.com>
2023-04-11 14:50:40 +0300

    Add task 32

---
+++ Sem5Task32/Program.cs
@@ -0,0 +1,41 @@
+﻿// Напишите программу замены элементов массива:
+// положительные элементы замените на 
+// соответствующие отрицательные, и наоборот.
+
+// Генерация одномерного массива
+int[] Gen1DArray(int len, int bot, int top)
+{
+    if (bot > top)
+    {
+        int buf = bot;
+        bot = top;
+        top = buf;
+    }
+
+    int[] res = new int[len];
+    for (int i = 0; i < len; i++)
+        res[i] = new Random().Next(bot, top + 1);
+    return res;
+}
+
+// Метод вывода массива в консоль
+void Print1DArray(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length - 1; i++)
+        Console.Write(arr[i] + ", ");
+    Console.WriteLine(arr[arr.Length - 1] + "]");
+}
+
+// Инвертирование элементов массива 
+void InverseArr(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+        arr[i] = arr[i] * (-1);
+}
+
+int[] testArr = Gen1DArray(10, -100, 100);
+Print1DArray(testArr);
+InverseArr(testArr);
+Print1DArray(testArr);
+
