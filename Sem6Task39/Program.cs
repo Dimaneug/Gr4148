b71c5bee29c69fa0343b22ae22c57b0ab3cda0a4
Dimaneug <dimaneug@gmail.com>
2023-04-17 09:31:00 +0300

    Add task 39

---
+++ Sem6Task39/Program.cs
@@ -0,0 +1,56 @@
+﻿// Перевернуть массив
+
+// Генерация одномерного массива
+int[] Gen1DArray(int len, int bot, int top)
+{
+    if (bot > top)
+    {
+        int buf = bot;
+        bot = top;
+        top = buf;
+    }
+
+    int[] res = new int[len];
+    for (int i = 0; i < len; i++)
+    {
+        res[i] = new Random().Next(bot, top);
+    }
+    return res;
+}
+
+// Метод вывода массива в консоль
+void Print1DArray(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length - 1; i++)
+        Console.Write(arr[i] + ", ");
+    Console.WriteLine(arr[arr.Length - 1] + "]");
+}
+
+// Разворачивает исходный массив
+void ReverseV1(int[] arr) 
+{
+    for (int i = 0; i < arr.Length/2; i++)
+    {
+        (arr[i], arr[arr.Length-1-i]) = (arr[arr.Length-1-i], arr[i]);
+    }
+}
+
+// Возвращает развёрнутый массив
+int[] ReverseV2(int[] arr)
+{
+    int[] newArr = new int[arr.Length];
+    for (int i = arr.Length-1; i >= 0; i--)
+    {
+        newArr[arr.Length-i-1] = arr[i];
+    }
+    return newArr;
+}
+
+int[] testArr = Gen1DArray(11, 0, 20);
+Print1DArray(testArr);
+int[] newArr = ReverseV2(testArr);
+Print1DArray(newArr);
+ReverseV1(testArr);
+Print1DArray(testArr);
+
