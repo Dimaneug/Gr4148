1cf4ba69cf4796a602d3571b0fcd14f748cab1b5
Dimaneug <dimaneug@gmail.com>
2023-04-17 09:41:06 +0300

    Add task 40

---
+++ Sem6Task40/Program.cs
@@ -0,0 +1,20 @@
+﻿// Принимает 3 числа и проверяет, 
+// может ли существовать треугольник с такими сторонами
+
+// Метод читает данные от пользователя
+int ReadData(string msg)
+{
+    Console.Write(msg);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+
+// Проверка существования треугольника по 3 сторонам
+bool CheckTriangleExist(int a, int b, int c)
+{
+    return (a + b > c) && (b + c > a) && (c + a > b);
+}
+
+int a = ReadData("a: ");
+int b = ReadData("b: ");
+int c = ReadData("c: ");
+Console.WriteLine(CheckTriangleExist(a,b,c)?"Существует":"Не существует");
