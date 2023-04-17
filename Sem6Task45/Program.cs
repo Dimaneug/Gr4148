e6abc008e6222a810f53764787698aeba3784f02
Dimaneug <dimaneug@gmail.com>
2023-04-17 10:22:55 +0300

    Add task 45

---
+++ Sem6Task45/Program.cs
@@ -0,0 +1,46 @@
+﻿// Создать копию одномерного массива 
+// с помощью поэлементного копирования
+
+// Генерация одномерного массива
+int[] Gen1DArray(int len, int bot, int top)
+{
+    if (bot > top)
+    {
+        int buf = bot;
+        bot = top;
+        top = buf;
+    }
+
+    int[] res = new int[len];
+    for (int i = 0; i < len; i++)
+    {
+        res[i] = new Random().Next(bot, top);
+    }
+    return res;
+}
+
+// Метод вывода массива в консоль
+void Print1DArray(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length - 1; i++)
+        Console.Write(arr[i] + ", ");
+    Console.WriteLine(arr[arr.Length - 1] + "]");
+}
+
+// Создание копии одномерного массива
+int[] CopyArr(int[] arr)
+{
+    int[] newArr = new int[arr.Length];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        newArr[i] = arr[i];
+    }
+    return newArr;
+}
+
+int[] testArr = Gen1DArray(10,0,20);
+Print1DArray(testArr);
+int[] newArr = CopyArr(testArr);
+testArr[0] = 99;
+Print1DArray(newArr);
