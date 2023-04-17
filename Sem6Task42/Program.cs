848dfa509675438bc9342784c121bcb584b93211
Dimaneug <dimaneug@gmail.com>
2023-04-17 09:57:12 +0300

    Add task 42

---
+++ Sem6Task42/Program.cs
@@ -0,0 +1,24 @@
+﻿// Преобразовать десятичное число в двоичное
+
+// Метод читает данные от пользователя
+int ReadData(string msg)
+{
+    Console.Write(msg);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+
+// Перевод из десятичной в двоичную
+string ConvertToBin(int num)
+{
+    string res = "";
+    while (num>0) 
+    {
+        res = (num%2).ToString() + res;
+        num /= 2;
+    }
+    return res;
+}
+
+int num = ReadData("Введите число: ");
+Console.WriteLine(ConvertToBin(num));
+
