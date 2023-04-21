7826835b1e9211856e1572d776207b246b03ca63
Dimaneug <dimaneug@gmail.com>
2023-04-21 17:40:08 +0300

    Add task 66

---
+++ Home9Task66/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задайте значения M и N. Напишите программу, 
+// которая найдёт сумму натуральных элементов 
+// в промежутке от M до N.
+
+// Метод читает данные от пользователя
+int ReadData(string msg)
+{
+    Console.Write(msg);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+
+// Возвращает сумму натуральных чисел из промежутка [m;n]
+int RecSumMN(int m, int n)
+{
+    return (m<=n)?m+RecSumMN(m+1,n):0;
+}
+
+int m = ReadData("Введите M: ");
+int n = ReadData("Введите N: ");
+Console.WriteLine((m>n)?RecSumMN(n,m):RecSumMN(m,n));
+
