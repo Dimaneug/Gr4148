1bcc14ac21cda0c76ee937fdf57f9ac8d1a5e195
Dimaneug <dimaneug@gmail.com>
2023-04-21 08:22:40 +0300

    Add task 55

---
+++ Sem8Task55/Program.cs
@@ -0,0 +1,84 @@
+﻿// Задайте двумерный массив. Напишите программу, 
+// которая заменяет строки на столбцы. В случае, 
+// если это невозможно, программа должна вывести 
+// сообщение для пользователя.
+
+// Метод читает данные от пользователя
+int ReadData(string msg)
+{
+    Console.Write(msg);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+
+// Генерация двумерного массива
+int[,] Gen2DArray(int m, int n, int bot, int top)
+{
+    if (bot > top)
+    {
+        int buf = bot;
+        bot = top;
+        top = buf;
+    }
+
+    int[,] res = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            res[i, j] += new Random().Next(bot, top);
+        }
+    }
+    return res;
+}
+
+// Печать двумерного массива
+void Print2DArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write(arr[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+// Возвращает транспонированную матрицу
+int[,] Transpose2DArray(int[,] arr)
+{
+    int[,] outArr = new int[arr.GetLength(0), arr.GetLength(1)];
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            outArr[j,i] = arr[i,j];
+        }
+    }
+    return outArr;
+}
+
+// Транспонирование исходной матрицы 
+void TransposeCurrent2DArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = i+1; j < arr.GetLength(1); j++)
+        {
+            int temp = arr[i,j];
+            arr[i,j] = arr[j,i];
+            arr[j,i] = temp;
+        }
+    }
+}
+
+int m = ReadData("Введите m: ");
+int n = ReadData("Введите n: ");
+int[,] testArr = Gen2DArray(m, n, 0, 50);
+Print2DArray(testArr);
+int[,] newArr = Transpose2DArray(testArr);
+Console.WriteLine();
+Print2DArray(newArr);
+Console.WriteLine();
+TransposeCurrent2DArray(testArr);
+Print2DArray(testArr);
