8c65ce0b61d2bf8c0106a44069f2dc9eb8add6e6
Dimaneug <dimaneug@gmail.com>
2023-04-21 17:08:11 +0300

    Add task 67

---
+++ Sem9Task67/Program.cs
@@ -0,0 +1,17 @@
+﻿// Почитать сумму цифр числа рекурсивно
+
+// Метод читает данные от пользователя
+int ReadData(string msg)
+{
+    Console.Write(msg);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+
+// Сумма цифр числа рекурсивно
+int SumOfDigits(int num)
+{
+    return (num > 0) ? num % 10 + SumOfDigits(num / 10) : 0;
+}
+
+int num = ReadData("Введите число: ");
+Console.WriteLine("Сумма цифр: " + SumOfDigits(num));
