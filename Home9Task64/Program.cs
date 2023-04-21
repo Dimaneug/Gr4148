826d6a91e5a9d966a173527116beb6db96e60e83
Dimaneug <dimaneug@gmail.com>
2023-04-21 17:36:23 +0300

    Add task 64

---
+++ Home9Task64/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задайте значение N. Напишите программу, 
+// которая выведет все натуральные числа в промежутке 
+// от 1 до N. Выполнить с помощью рекурсии.
+
+// Метод читает данные от пользователя
+int ReadData(string msg)
+{
+    Console.Write(msg);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+
+// Вывод чисел от 1 до N рекурсивно
+string LineGenRec(int numN)
+{
+    return (numN>0)?LineGenRec(numN-1)+numN+" ":String.Empty;
+}
+
+int numN = ReadData("Введите N: ");
+Console.WriteLine(LineGenRec(numN));
