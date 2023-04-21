ef671fa62a88c90159d5e09848e1b663ca25bf67
Dimaneug <dimaneug@gmail.com>
2023-04-21 16:34:57 +0300

    Add task 63

---
+++ Sem9Task63/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задайте значение N. Напишите программу, 
+// которая выведет все натуральные числа в промежутке от N до 1. 
+// Выполнить с помощью рекурсии.
+
+// Метод читает данные от пользователя
+int ReadData(string msg)
+{
+    Console.Write(msg);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+
+// Вывод чисел от N до 1 рекурсивно
+void LineGenRec(int numN)
+{
+    Console.Write(numN + " ");
+    if (numN > 0)
+    {
+        LineGenRec(numN-1); 
+    }
+}
+
+int numN = ReadData("Введите N: ");
+LineGenRec(numN);
+Console.WriteLine();
