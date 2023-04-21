8a368d1476cb4e9259820465c1d1416cfbc02945
Dimaneug <dimaneug@gmail.com>
2023-04-21 17:28:19 +0300

    Add task 69

---
+++ Sem9Task69/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая будет принимать два числа a и b.
+// Возведите число a в степень b при помощт рекурсии.
+
+// Метод читает данные от пользователя
+int ReadData(string msg)
+{
+    Console.Write(msg);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+
+// Возведение a в степень b рекурсивно
+long APowB(int a, int b)
+{
+    if (b == 1) return a;
+    if (b == 0) return 1;
+    return (b == 2) 
+            ? a * a 
+            : APowB(a, (b % 2 == 0) 
+                        ? b / 2 
+                        : b / 2 + 1) * APowB(a, b / 2);
+}
+
+int a = ReadData("Введите a: ");
+int b = ReadData("Введите b: ");
+Console.WriteLine(APowB(a, b));
